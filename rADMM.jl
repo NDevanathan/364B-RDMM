4cd1da49dc5055236248103b6001860d177c492e
Nikhil Devanathan <nikhil.devanathan@gmail.com>
2022-05-12 15:56:33 -0700

    initial setting up stuff

---
+++ rADMM.jl
@@ -0,0 +1,27 @@
+using LinearAlgebra
+using Random
+using Distributed
+
+function rADMM_LS(A,b,maxiter)
+    lambda = 0
+    x = 0
+    return x,lambda
+end
+
+function rADMM_Ridge(A,b,maxiter)
+    lambda = 0
+    x = 0
+    return x,lambda
+end
+
+function rADMM_QuadReg(A,b,maxiter)
+    lambda = 0
+    x = 0
+    return x,lambda
+end
+
+function rADMM_SOCP(A,b,maxiter)
+    lambda = 0
+    x = 0
+    return x,lambda
+end
