8dbd017cb977fd980db64b24ab0b798221cb30d1
Nikhil Devanathan <nikhil.devanathan@gmail.com>
2022-05-13 11:14:28 -0700

    set up a couple more function shells

--- rADMM.jl
+++ rADMM.jl
@@ -25,3 +25,14 @@ function rADMM_SOCP(A,b,maxiter)
     x = 0
     return x,lambda
 end
+
+function preprocessA(A)
+    #fast walsh goes here
+
+    return x,lambda
+end
+
+function preprocessb(b)
+
+    return x,lambda
+end
