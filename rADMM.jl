da2c7d789a234ece3014a822e9cccdae50d8966b
Nikhil Devanathan <nikhil.devanathan@gmail.com>
2022-05-13 11:15:27 -0700

    set up a couple more function shells

--- rADMM.jl
+++ rADMM.jl
@@ -2,37 +2,37 @@ using LinearAlgebra
 using Random
 using Distributed
 
-function rADMM_LS(A,b,maxiter)
+function rADMM_LS(A,b,N,maxiter)
     lambda = 0
     x = 0
     return x,lambda
 end
 
-function rADMM_Ridge(A,b,maxiter)
+function rADMM_Ridge(A,b,N,maxiter)
     lambda = 0
     x = 0
     return x,lambda
 end
 
-function rADMM_QuadReg(A,b,maxiter)
+function rADMM_QuadReg(A,b,N,maxiter)
     lambda = 0
     x = 0
     return x,lambda
 end
 
-function rADMM_SOCP(A,b,maxiter)
+function rADMM_SOCP(A,b,N,maxiter)
     lambda = 0
     x = 0
     return x,lambda
 end
 
-function preprocessA(A)
+function preprocessA(A,N)
     #fast walsh goes here
 
     return x,lambda
 end
 
-function preprocessb(b)
+function preprocessb(b,N)
 
     return x,lambda
 end
