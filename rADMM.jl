4886459f9d592dd258954c7a2e611e3938cdcff8
Logan Bell <epicycloids@gmail.com>
2022-05-13 15:28:13 -0700

    Added utility function for Ridge, added preprocessing to radmm_ls and radmm_ridge, eliminated the use of inv()

--- rADMM.jl
+++ rADMM.jl
@@ -1,7 +1,7 @@
 using LinearAlgebra
 using Random
 using Statisitics
-using Distributed;
+using Distributed
 using Dagger
 using FFTW
 
@@ -10,12 +10,13 @@ function radmm_ls(A,b,N,maxiter,mu)
     n = size(A,1)
     d = size(A,2)
     
+    SA, Sb = preprocessAb_ls(A,b,N)
     x = [zeros(d) for i=1:N]
     lambda = [zeros(d) for i=1:N]
     
     for k=1:maxiter
         for i=1:N
-            x[i] = Dagger.@spawn inv(SA[i]'*SA[i])*(SA[i]'*Sb[i]-lambda[i])
+            x[i] = Dagger.@spawn (SA[i]'*SA[i]) \ (SA[i]'*Sb[i]-lambda[i])
             lambda[i] = Dagger.@spawn lambda[i]+mu*A'*A*(x[i]-mean(x))
         end
     end
@@ -32,12 +33,13 @@ function radmm_ridge(A,b,N,maxiter)
     n = size(A,1)
     d = size(A,2)
     
+    SAt = preprocessA_ridge(A,N)
     y = [zeros(n) for i=1:N]
     lambda = [zeros(n) for i=1:N]
     
     for k=1:maxiter
         for i=1:N
-            y[i] = Dagger.@spawn inv(SAt[i]'*SAt[i]+I(n)/N)*(b/N-lambda[i])
+            y[i] = Dagger.@spawn (SAt[i]'*SAt[i]+I(n)/N) \ (b/N-lambda[i])
             lambda[i] = Dagger.@spawn lambda[i]+mu*(A*A'+I(n)/N)*(y[i]-mean(y))
         end
     end
@@ -59,7 +61,7 @@ function radmm_qr(A,b,N,maxiter)
     
     for k=1:maxiter
         for i=1:N
-            y[i] = Dagger.@spawn inv(SAt[i]'*SAt[i]+I(n)/N)*(b/N-lambda[i])
+            y[i] = Dagger.@spawn (SA[i]'*SA[i]+I(n)/N) \ (b/N-lambda[i])
             lambda[i] = Dagger.@spawn lambda[i]+mu*(A*A'+I(n)/N)*(y[i]-mean(y))
         end
     end
@@ -76,7 +78,8 @@ Utility function to preprocess A and b for RDMM LS
 Inputs:
     A - A matrix of size n by d
     b - A column vector of size n
-    N - The number of agents. N should divide n (the number of rows in A).
+    N - The number of agents. N should divide n (the number of rows in A),
+        and should also satisfy N <= n/d.
 
 Outputs:
     SA - List of all S_i*A
@@ -86,10 +89,12 @@ function preprocessAb_ls(A,b,N)
     # TO DO: Allow for more general choices of N
     # TO DO: Check correctness
     n = size(A, 1)
+    d = size(A, 2)
     if n%N != 0
         throw(DimensionMismatch("N must divide the number of rows of A"))
+    elseif N > n/d
+        throw(DimensionMismatch("N can be at most n/d"))
     end
-    d = size(A, 2)
     
     dividedindices = Iterators.partition(randperm(n), Int(ceil(n/N)))
     D = Diagonal(sign.(rand(n) .- 0.5))
@@ -107,6 +112,36 @@ function preprocessAb_ls(A,b,N)
 end
 
 """
+Utility function to preprocess A for RDMM Ridge
+Inputs:
+    A - A matrix of size n by d.
+    N - The number of agents. N should divide n (the number of rows in A).
+
+Outputs:
+    SAt - List of all S_i*At.
+"""
+function preprocessA_ridge(A,N)
+    # TO DO: Allow for more general choices of N
+    # TO DO: Check correctness
+    n = size(A, 1)
+    d = size(A, 2)
+    if n%N != 0
+        throw(DimensionMismatch("N must divide the number of rows of A"))
+    end
+    
+    dividedindices = Iterators.partition(randperm(n), Int(ceil(n/N)))
+    D = Diagonal(sign.(rand(n) .- 0.5))
+    HDAt = FFTW.r2r(D*A', FFTW.DHT, 1)
+    
+    SAt = []
+    for indexcollection in dividedindices
+        push!(SAt, HDAt[indexcollection, :] / sqrt(N*length(indexcollection)))
+    end
+    
+    return SAt
+end
+
+"""
 Utility function to preprocess A and b for RDMM Regularized LS
 Inputs:
     A - A matrix of size n by d
@@ -117,6 +152,25 @@ Outputs:
     Sb - List of all S_i*b.
 """
 function preprocessAb_quadreg(A,b)
-    return preprocessAb_ls(A,b,2)
+    # TO DO: Check to make sure we don't need n >= 2*d
+    n = size(A, 1)
+    d = size(A, 2)
+    if n%2 != 0
+        throw(DimensionMismatch("N must divide the number of rows of A"))
+    end
+    
+    dividedindices = Iterators.partition(randperm(n), Int(ceil(n/2)))
+    D = Diagonal(sign.(rand(n) .- 0.5))
+    HDA = FFTW.r2r(D*A, FFTW.DHT, 1)
+    HDb = FFTW.r2r(D*b, FFTW.DHT)
+    
+    SA = []
+    Sb = []
+    for indexcollection in dividedindices
+        push!(SA, HDA[indexcollection, :] / sqrt(N*length(indexcollection)))
+        push!(Sb, HDb[indexcollection, :] / sqrt(N*length(indexcollection)))
+    end
+    
+    return SA, Sb
 end
 
